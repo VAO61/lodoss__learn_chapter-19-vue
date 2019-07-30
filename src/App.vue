f5e975506b80f4257feb2f35011ca5a59ac5e6a3
VAO61 <vao.frontend@gmail.com>
2019-07-30 10:41:29 +0300

    Added temporary component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <TemporaryIncludingAllComponent />
     <SomeNewComponent msg="New component" msgButton="Button" />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import SomeNewComponent from "./components/SomeNewComponent.vue";
+import TemporaryIncludingAllComponent from "./components/TemporaryIncludingAllComponent.vue";
 
 export default {
   name: "app",
   components: {
     // HelloWorld
-    SomeNewComponent
+    SomeNewComponent,
+    TemporaryIncludingAllComponent
   }
 };
 </script>
