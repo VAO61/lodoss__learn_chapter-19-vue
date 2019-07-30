fbd8b6e60da950e03571247a0d41c6043415b4bd
VAO61 <vao.frontend@gmail.com>
2019-07-30 13:24:40 +0300

    v0.0.0.1 --pre-alpha

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,32 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <TemporaryIncludingAllComponent />
-    <SomeNewComponent msg="New component" msgButton="Button" />
+    <!-- <SomeNewComponent msg="New component" msgButton="Button" /> -->
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import SomeNewComponent from "./components/SomeNewComponent.vue";
+// import SomeNewComponent from "./components/SomeNewComponent.vue";
 import TemporaryIncludingAllComponent from "./components/TemporaryIncludingAllComponent.vue";
 
 export default {
   name: "app",
   components: {
     // HelloWorld
-    SomeNewComponent,
+    // SomeNewComponent,
     TemporaryIncludingAllComponent
   }
 };
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
