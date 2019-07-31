34d9051f5620a329c96dcdb02af588bff464eec5
VAO61 <vao.frontend@gmail.com>
2019-07-31 18:17:25 +0300

    v0.0.1.1 --pre-alpha

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,19 +1,29 @@
 <template>
   <section class="search container">
     <div class="search__form form">
-      <select v-model="type.model">
-        <label for="type">Type</label>
-        <option v-for="option in type.options" v-bind:key="option" :value="option">{{option}}</option>
-      </select>
-      <select v-model="language.model">
-        <option v-for="option in language.options" :key="option" :value="option">{{option}}</option>
-      </select>
+      <div>
+        <!-- <select v-model="type.model" id="selectType" onchange="testF()"> -->
+        <select v-model="type.model" id="selectType">
+          <option v-for="option in type.options" v-bind:key="option" :value="option">{{option}}</option>
+        </select>
+        <label for="selectType">Type</label>
+      </div>
+      <div>
+        <select v-model="language.model" id="selectLanguage">
+          <option v-for="option in language.options" :key="option" :value="option">{{option}}</option>
+        </select>
+        <label for="selectLanguage">Language</label>
+      </div>
       <!-- TODO: v-model="readme" -->
     </div>
   </section>
 </template>
 
 <script>
+// const testF = () => {
+//   document.querySelector("select").style.backgroundColor = "red";
+// };
+// testF();
 export default {
   data() {
     return {
