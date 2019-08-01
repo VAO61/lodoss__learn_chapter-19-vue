3eb51e0ea638513514b4d3f5efb23917716e6a8e
VAO61 <vao.frontend@gmail.com>
2019-08-01 19:34:01 +0300

    v0.0.2.0 --pre-alpha

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -2,13 +2,16 @@
   <main class="main container result-list">
     <button class="btn btn_red"></button>
     <button class="btn btn_blue"></button>
-    <div class="result-list__container">
-      <div class="result-list__item">
-        <div class="result-list__">1</div>
-        <div class="result-list__">2</div>
-        <div class="result-list__">3</div>
+    <!--
+      v-bind:class="isTile ? `${listTile}__` : `${list}__`"
+    -->
+    <div v-bind:class="isTile ? `${listTile}__container` : `${list}__container`">
+      <div v-bind:class="isTile ? `${listTile}__item` : `${list}__item`">
+        <div v-bind:class="isTile ? `${listTile}__info` : `${list}__info`"></div>
       </div>
+      <!-- .result-list -->
     </div>
+    <!-- :class="ListMode ? TileTheme : RowsTheme" -->
   </main>
 </template>
 
@@ -26,8 +29,20 @@
 //   })
 // }
 // <div className={ isPlate? 'plate' : 'stroke'}>dsadsa</div>
-export default {};
+
+/**
+ * For <input> -> <button>
+ * https://ru.vuejs.org/v2/guide/transitions.html#%D0%9F%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B4%D1%8B-%D0%BC%D0%B5%D0%B6%D0%B4%D1%83-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%B0%D0%BC%D0%B8
+ */
+export default {
+  data() {
+    return {
+      list: "result-list",
+      listTile: "result-list_tile"
+    };
+  }
+};
 </script>
 
-<style>
+<style lang="scss">
 </style>
